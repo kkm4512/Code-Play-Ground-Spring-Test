f9c30a7e148aae5e7e065461ea3d6f9e69664f9d
kkm4512 <nayoun440@naver.com>
2024-11-05 13:29:46 +0900

    WebSocket Test

---
+++ src/main/java/com/example/codplaygroundspringtest/websocket/WebSocketHandler.java
@@ -0,0 +1,44 @@
+package com.example.codplaygroundspringtest.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j(topic = "WebSocketHandler")
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<String, WebSocketSession>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        CLIENTS.put(session.getId(), session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        CLIENTS.remove(session.getId());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info(session.getHandshakeHeaders().toString());
+        String id = session.getId();  //메시지를 보낸 아이디
+        log.info("id {}", id);
+        CLIENTS.entrySet().forEach( arg-> {
+            if(!arg.getKey().equals(id)) {  //같은 아이디가 아니면 메시지를 전달합니다.
+                try {
+                    arg.getValue().sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
